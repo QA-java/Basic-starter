099338c753faf8cc4f02d8fcf32aa712cfbfbc73
NadeemAhamad-QA <nadeemahamadqa@gmail.com>
2024-01-28 23:06:08 +0530

    Operator session part1

---
+++ Jan19.java
@@ -0,0 +1,11 @@
+public class Jan19{
+    public static void main(String... arg){
+        int a;
+        a = 100;
+        System.out.println(a);
+        
+        a=223;
+        System.out.println(a);
+        
+    }
+}
---
+++ OperatorDemo.java
@@ -0,0 +1,41 @@
+import javax.security.auth.x500.X500Principal;
+
+public class OperatorDemo {
+
+    public static void main(String... arg) {
+        int a = 100, b = 2, sum = a + b, sub = a - b, multi = a * b, devide = a / b, remainder = a % b;
+        System.out.println("Value of a =" + (a));
+        System.out.println("Value of b =" + (b));
+        System.out.println("sum of a+b =" + (sum));
+        System.out.println("Value of a-b =" + (sub));
+        System.out.println("Value of a*b =" + (multi));
+        System.out.println("Value of a/b =" + (devide));
+        System.out.println("remainder of a/b =" + (remainder));
+
+        System.out.println(a > b);
+        System.out.println(a + b < a / b);
+        System.out.println(a != b);
+        System.out.println(a == b);
+
+        System.out.println(multi + sum);
+        System.out.println((a * b) + (a + b));
+
+        boolean bu = multi + sum != (a * b) + (a + b);
+
+        System.out.println(bu);
+
+        System.out.println(" ****************Section 2***************");
+
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println(x); //ture
+        System.out.println(y); //false
+        System.out.println(x && y); //false
+        System.out.println(x || y); //true
+        System.out.println(!x); // false
+        System.out.println(!y); //true
+
+    }
+
+}
