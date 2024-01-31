326b604ec821b1e28e83261c66d9ed41c3aeaa58
NadeemAhamad-QA <nadeemahamadqa@gmail.com>
2024-01-31 12:27:35 +0530

    WeekExample

---
+++ WeekExampleConditional.java
@@ -0,0 +1,36 @@
+
+
+public class WeekExampleConditional {
+
+    public static void main(String... arg){
+
+       int dayOfWeek = 0;
+
+        if (dayOfWeek == 01){
+            System.out.println("This is Sunday");
+        } 
+        else if (dayOfWeek == 02){
+            System.out.println("This is Monday");
+        }
+        if (dayOfWeek == 03){
+            System.out.println("This is Tuesday");
+        } 
+        else if (dayOfWeek == 04){
+            System.out.println("This is Wednesday");
+        }
+        if (dayOfWeek == 05){
+            System.out.println("This is Thirsday");
+        } 
+        else if (dayOfWeek == 06){
+            System.out.println("This is Friday");
+        }
+        else if(dayOfWeek == 07){
+            System.out.println("This is Saturday");
+        }
+        else if(dayOfWeek == 10){
+            System.out.println("Value is invalid");
+        }
+
+    }
+    
+}
